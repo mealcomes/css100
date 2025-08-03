6dcc0cf2f5eabf357df27954ef5424e48ef03439
mealcomes <2013213920@qq.com>
2025-08-03 15:08:43 +0800

    feat: 效果展示界面对每个示例添加重新加载按钮

--- index.js
+++ index.js
@@ -50,9 +50,18 @@ const addIframe = () => {
     title.href = dataSrc;
     title.target = '_blank';
 
+    const reload = document.createElement('div');
+    reload.innerHTML =
+        '<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>';
+    reload.className = 'reload';
+    reload.onclick = () => {
+        iframe.src += '';
+    };
+
     const data = document.createElement('div');
     data.className = 'data';
     data.appendChild(title);
+    data.appendChild(reload);
 
     const div = document.createElement('div');
     div.className = 'item';
